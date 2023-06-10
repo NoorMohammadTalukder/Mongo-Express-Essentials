7c46f556d0e1f91e5713783c4c401b1baad92efc
NoorMohammadTalukder <noor11809027@gmail.com>
2023-06-11 02:34:18 +0600

    read-update-delete student

--- src/controller/StudentController.js
+++ src/controller/StudentController.js
@@ -1,12 +1,54 @@
 const studentModel = require("../models/StudentsModel");
 
+//insert student data
 exports.insertStudent = (req, res) => {
   let body = req.body;
   studentModel.create(body, (err, data) => {
     if (err) {
-      res.status(200).json({ message: "data insert failed", error: err });
+      res.status(400).json({ message: "data insert failed", error: err });
     } else {
-      res.status(201).json({ message: "data insert successful", data: data });
+      res.status(200).json({ message: "data insert successful", data: data });
     }
   });
 };
+
+// read student data
+exports.readAllStudent = (req, res) => {
+  let query = {};
+  let projection = "name roll class remarks";
+
+  studentModel.find(query, projection, (err, data) => {
+    if (err) {
+      res.status(400).json({ message: "data reading failed", error: err });
+    } else {
+      res.status(200).json({ message: "data reading successful", data: data });
+    }
+  });
+};
+
+exports.updateStudent=(req,res)=>{
+    let id=req.params.id;
+    let query={_id:id};
+    let body=req.body;
+    
+    studentModel.updateOne(query,body,(err,data)=>{
+        if (err) {
+            res.status(400).json({ message: "data update failed", error: err });
+          } else {
+            res.status(200).json({ message: "data update successful", data: data });
+          }
+    })
+}
+
+exports.deleteStudent=(req,res)=>{
+    let id=req.params.id;
+    let query={_id:id};
+
+    studentModel.remove(query,(err,data)=>{
+        if(err){
+            res.status(400).json({"message":"data deletion failed",error:err});
+        }else{
+            res.status(200).json({"message":"data deletion successful",data:data})
+        }
+    })
+}
--- src/routes/api.js
+++ src/routes/api.js
@@ -3,6 +3,9 @@ const studentController=require("../controller/StudentController")
 const router=  express.Router();
 
 //---------APIs
-router.post("/insert-student",studentController.insertStudent)
+router.post("/insert-student",studentController.insertStudent);
+router.get("/read-all-student",studentController.readAllStudent);
+router.post("/update-student/:id",studentController.updateStudent);
+router.post("/delete-student/:id",studentController.deleteStudent);
 
 module.exports=router;
