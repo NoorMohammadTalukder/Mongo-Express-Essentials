f4dd7cc5a8103ac5961fcf2ae61a49b9d42bea87
NoorMohammadTalukder <noor11809027@gmail.com>
2023-06-11 14:55:01 +0600

    version key disable, default value for student model

--- src/models/StudentsModel.js
+++ src/models/StudentsModel.js
@@ -4,8 +4,9 @@ const studentSchema=mongoose.Schema({
     name:String,
     roll:String,
     class:String,
-    remarks:String,
-})
+    remarks:{type:String,default:"No comments"},
+    date:{type:Date,default:Date.now}
+},{versionKey:false})
 
 const studentModel=mongoose.model("students",studentSchema);
 module.exports=studentModel;
